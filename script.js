998dc9985579dbbd2937c4d5ef3943d5389b353f
atsushi0919 <hatake0919@yahoo.co.jp>
2022-05-10 15:58:32 +0900

    Hit and Blow 判定追加

--- script.js
+++ script.js
@@ -1,18 +1,61 @@
 'use strict'
 
-let count = 10
-let ary = [...Array(10)].map((_, i) => i)
-let answer = random(ary, 4)
+// 0 から 9 までの数字からランダムに 4 個選ぶ
+const n = 4
+const ary = [...Array(10)].map((_, i) => i)
+const answer = random(ary, 4)
+// 回答数
+const maxCount = 10
+let count = 0
 
 // 答え表示
 document.getElementById('answer').textContent = answer
+// 回答フォーム初期化
+changeNum()
+
+function changeNum() {
+  console.log('Change Number')
+
+  // 選択した番号が全て違うか
+  const numbers = getSelectedNumbers()
+  const isAllUnique = numbers.length === new Set(numbers).size
+
+  // ボタン有効・無効切り替え
+  const btn = document.getElementById('okBtn')
+  if (isAllUnique) {
+    btn.removeAttribute('disabled')
+  } else {
+    btn.setAttribute('disabled', true)
+  }
+  console.log(numbers)
+  console.log(isAllUnique)
+}
+
+function getSelectedNumbers() {
+  return [
+    document.getElementById('selectedNum0').value,
+    document.getElementById('selectedNum1').value,
+    document.getElementById('selectedNum2').value,
+    document.getElementById('selectedNum3').value,
+  ].map(Number)
+}
 
 function pushOk() {
   console.log('OK')
-}
+  const numbers = getSelectedNumbers()
 
-function pushReset() {
-  console.log('RESET')
+  let blow = 0
+  let hit = 0
+  for (let i = 0; i < n; i++) {
+    if (answer[i] == numbers[i]) {
+      hit++
+    } else if (numbers.includes(answer[i])) {
+      blow++
+    }
+  }
+  console.log(answer)
+  console.log(numbers)
+  console.log(hit, blow)
 }
 
 function random(array, n) {
